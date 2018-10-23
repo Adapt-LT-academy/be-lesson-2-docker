f99f16669a35af6c33cd79d298745ad7443730b8
Irmantas Puodžiukas <irmantas@adapt.dk>
2018-10-23 17:37:49 +0300

    Udate scripts and docker-compose files for deeper docker compose part

--- 05.deeper-into-docker-compose/05.deeper-into-docker-compose.sh
+++ 05.deeper-into-docker-compose/05.deeper-into-docker-compose.sh
@@ -7,7 +7,7 @@ pause(){
 }
 
 clear
-echo ""
+echo "Contents of our index files for our previously created containers:"
 echo
 cat ./apache/index.html
 echo
@@ -16,34 +16,51 @@ cat ./nginx/index.html
 pause
 
 clear
-echo ""
+echo "Contents of basic Docker Compose configuration file:"
 pause
 cat ./docker-compose.yml
 pause
 
 clear
-echo ""
+echo "Start this configuration:"
+echo
+echo "COMMAND:"
+echo
+echo "docker-compose up -d"
 pause
-
 docker-compose up -d
 pause
 
 clear
-echo ""
+echo "Lets add our index.html files for each container:"
+pause
 cat ./docker-compose.secondary.yml
 pause
 
 clear
-echo ""
+echo "Start this configuration:"
+echo
+echo "COMMAND:"
+echo
+echo "docker-compose  -f docker-compose.secondary.yml up -d"
+pause
 docker-compose -f docker-compose.secondary.yml up -d
 pause
 
 clear
-echo ""
+echo "Lets add additional service which allows creation of local domains for containers:"
+pause
 cat ./docker-compose.third.yml
 pause
 
 clear
-echo ""
+echo "Start this configuration:"
+echo
+echo "COMMAND:"
+echo
+echo "docker-compose -f docker-compose.third.yml up -d"
+pause
 docker-compose -f docker-compose.third.yml up -d
 pause
+
+docker stop $(docker ps -aq) > /dev/null
